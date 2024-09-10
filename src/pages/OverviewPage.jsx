a3343c314db22dea3606837432e44f3bee3e8817
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-09-10 16:56:40 +0300

    feat: Implement Products Page

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -16,9 +16,9 @@ const OverviewPage = () => {
         {/* STATS */}
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 200 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 1.5 }}
         >
           <StatCard
             name="Total Sales"
