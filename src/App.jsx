c84fa87662333520a558e98587fccab95ac4804c
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-09-11 19:38:17 +0300

    feat: add orders page

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/common/Sidebar";
 import UserPage from "./pages/UserPage";
 import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
 
 function App() {
   return (
     <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
-
       {/* BG */}
       <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"/>
-        <div className="absolute inset-0 backdrop-blur-sm"/>
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
 
       <Sidebar />
@@ -22,7 +22,8 @@ function App() {
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UserPage />} />
-        <Route path='/sales' element={<SalesPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
       </Routes>
     </div>
   );
